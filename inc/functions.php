7ad6e180673d224aeb860df7f37b93f5928d24f8
Chizzy <chizoba.princess@gmail.com>
2019-08-06 17:03:03 -0400

    created quotes array

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,72 @@
+<?php
 // PHP - Random Quote Generator
 
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
 
+$quotes = [
+    [
+        'quote' => "We should constantly be aspiring to reach higher and higher and higher. We should never be comfortable where we are.",
+        'source' => "Lauryn Hill",
+        'citation' => "Academy of Achievement",
+        'year' => 2000
+    ],
+    [
+        'quote' => "Musicians don’t retire; they stop when there’s no more music in them.",
+        'source' => "Louis Armstrong",
+        'citation' => "The Observer",
+        'year' => 1968
+    ],
+    [
+        'quote' => "Music does a lot of things for a lot of people. It’s transporting, for sure. It can take you right back, years back, to the very moment certain things happened in your life. It’s uplifting, it’s encouraging, it’s strengthening.",
+        'source' => "Aretha Franklin",
+    ],
+    [
+        'quote' => "We all must follow a different path to let our light shine, and that's what makes us so unpredictable and unique.",
+        'source' => "John Legend",
+        'citation' => "'Let every child's light shine', CNN.com",
+        'year' => 2015
+    ],
+    [
+        'quote' => "Don't wait for the stars to align, reach up and rearrange them the way you want...create your own constellation.",
+        'source' => "Pharrell",
+    ],
+    [
+        'quote' => "We all have ability. The difference is how we use it.",
+        'source' => "Stevie Wonder",
+        'citation' => "'The Story of Stevie Wonder', Book by James Haskins",
+        'year' => 1976
+    ],
+    [
+        'quote' => "You are only as beautiful as the many beautiful things you do for others without expectation.",
+        'source' => "Janelle Monae",
+        'citation' => "Huffington Post",
+        'year' => 2012
+    ],
+    [
+        'quote' => "If everything was perfect, you would never learn and you would never grow.",
+        'source' => "Beyoncé",
+        'citation' => "Interview Magazine",
+        'year' => 2001
+    ],
+    [
+        'quote' => "You can’t just sit there and wait for people to give you that golden dream. You’ve got to get out there and make it happen for yourself.",
+        'source' => "Diana Ross",
+        'citation' => "Jet Magazine, Vol. 67, No. 2",
+        'year' => 1985
+    ],
+    [
+        'quote' => "Knowledge speaks, but wisdom listens.",
+        'source' => "Jimi Hendrix",
+    ]
+];
+
+//var_dump($quotes);
 
 
-// Create the getRandomQuuote function and name it getRandomQuote
+// Create the getRandomQuote function and name it getRandomQuote
 
 
 
-// Create the printQuote funtion and name it printQuote
+// Create the printQuote function and name it printQuote
+?>
